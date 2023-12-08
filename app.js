c18822bc840146ddc9c18f745aca26b7a0c39504
Pedro Pablo <pmontesarroyave@gmail.com>
2023-12-08 14:18:22 -0500

    el bot esta funcionadon

--- app.js
+++ app.js
@@ -4,7 +4,55 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+const flowDefautl = addKeyword('')
+    .addAnswer(
+        [
+            'Esperame un momento',
+        ],
+        { capture: true },
+        (ctx) => {
+            console.log(`ctx`, ctx);
+        }
+    )
+
+const flowDudas = addKeyword('')
+    .addAnswer(
+        [
+
+        ],
+        { capture: true },
+        (ctx) => {
+            console.log(`ctx`, ctx);
+        }
+    )
+
+
+const flowSi = addKeyword(['si', 'Si', 'bueno', 'sí'])
+    .addAnswer(
+        [
+            '¡Genial! Hacer tu pedido con Domi Burguer es fácil y rápido:',
+            ' Ingresa a:  *https://domiburguer.com*',
+            'Cuando llenes el formulario te llegara un mensaje de *confirmacion* al whatsApp',
+            'despues solo es esperar'
+        ],
+        { capture: true },
+        (ctx) => {
+            console.log(`ctx`, ctx);
+        },
+    )
+
+const flowNo = addKeyword(['no', 'No'])
+    .addAnswer(
+        [
+            'Tranqui, siempre estaremos aqui para atenderte por este medio ✨\n',
+            '¿Que deseas ordenar el día de hoy?',
+        ],
+        { capture: true },
+        (ctx) => {
+            console.log(`ctx`, ctx);
+        },
+        [flowDefautl]
+    )
 
 
 const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
@@ -13,21 +61,22 @@ const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
     ],
     null,
     null,
-    [flowSecundario]
+    [flowDefautl]
 )
 
 const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido al bot de Domi Burguer')
+    .addAnswer('👋 Hola, bienvenido a Domi Burguer')
     .addAnswer(
         [
             '*¡Nos hemos actualizado!*',
-            'Ahora puedes hacer tu pedido de una manera más ágil. Ingresando a:  https://domiburguer.com',
-            'Si deseas hablar con una de nuestras asistentes envía el número 1',
+            'Ahora puedes hacer tu pedido de una manera más ágil',
+            '¿Quieres saber cómo?\n',
+            'Escribe:  *si*  /  *no*   para tu respuesta',
         ],
         null,
         null,
         // [flowDocs, flowGracias, flowTuto, flowDiscord]
-        [flowGracias, flowSecundario]
+        [flowGracias, flowSi, flowNo]
     )
 
 const main = async () => {
